e840da596cfbac43cbd22d67ea563e44ed3ff2ca
Eashir <eashir.arafat@gmail.com>
2018-08-19 12:56:14 -0400

    Icons updated

--- Kontrast/CountdownTimer.swift
+++ Kontrast/CountdownTimer.swift
@@ -1,42 +1,45 @@
+////
+////  CountdownTimer.swift
+////  Kontrast
+////
+////  Created by Eashir Arafat on 7/11/18.
+////  Copyright © 2018 Eashir Arafat. All rights reserved.
+////
 //
-//  CountdownTimer.swift
-//  Kontrast
+//import Foundation
+//import SwiftyUserDefaults
 //
-//  Created by Eashir Arafat on 7/11/18.
-//  Copyright © 2018 Eashir Arafat. All rights reserved.
-//
-
-import Foundation
-import SwiftyUserDefaults
-
-class CountdownTimer {
-	
-	var totalTime = Int(Defaults[.timerDuration])
-	var countDownTimer: Timer!
-	
-	func startTimer( completion: ((Bool) -> Void)?) { 
-		countDownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
-	}
-	
-	@objc func updateTime() {
-//		timerLabel.text = "\(timeFormatted(totalTime))"
-		
-		if totalTime != 0 {
-			totalTime -= 1
-		} else {
-			endTimer()
-		}
-	}
-	
-	func endTimer() {
-		countDownTimer.invalidate()
-	}
-	
-	func timeFormatted(_ totalSeconds: Int) -> String {
-		let seconds: Int = totalSeconds % 60
-		let minutes: Int = (totalSeconds / 60) % 60
-		//     let hours: Int = totalSeconds / 3600
-		return String(format: "%02d:%02d", minutes, seconds)
-	}
-	
-}
+//class CountdownTimer: Timer {
+//	
+//	init(time: Int) {
+//		self.time = time
+//	}
+//	
+//	var totalTime = Int(Defaults[.timerDuration])
+//	
+//	func startTimer( completion: ((Bool) -> Void)?) { 
+//		countDownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+//	}
+//	
+//	@objc func updateTime() {
+////		timerLabel.text = "\(timeFormatted(totalTime))"
+//		
+//		if totalTime != 0 {
+//			totalTime -= 1
+//		} else {
+//			endTimer()
+//		}
+//	}
+//	
+//	func endTimer() {
+//		countDownTimer.invalidate()
+//	}
+//	
+//	func timeFormatted(_ totalSeconds: Int) -> String {
+//		let seconds: Int = totalSeconds % 60
+//		let minutes: Int = (totalSeconds / 60) % 60
+//		//     let hours: Int = totalSeconds / 3600
+//		return String(format: "%02d:%02d", minutes, seconds)
+//	}
+//	
+//}
